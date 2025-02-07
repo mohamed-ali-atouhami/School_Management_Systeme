2877d06b99b4eddc929cc7c92991a2235f194fd1
Mohamed ali <mohamedaligamer23@gmail.com>
2025-02-07 22:34:20 +0100

    initial commit

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* Sidebar */}
+      <div className="w-[15%] md:w-[8%] lg:w-[16%] xl:w-[15%] bg-red-200 p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+            <Image src="/LogoSchool.png" alt="logo" width={40} height={40} />
+          <span className="hidden lg:block">SchoolMed</span>
+        </Link>
+      </div>
+      {/*Right Side*/}
+      <div className="w-[85%] md:w-[92%] lg:w-[84%] xl:w-[85%] bg-gray-900 text-white p-4">
+        right side
+      </div>
+
+    </div>
+
+
+  );
+}
