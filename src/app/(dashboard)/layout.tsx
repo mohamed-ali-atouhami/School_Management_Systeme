7c86a62370d224f61d39103bd281c3b29026e2b8
Mohamed ali <mohamedaligamer23@gmail.com>
2025-02-08 17:25:02 +0100

    improve-sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -9,16 +11,20 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* Sidebar */}
-      <div className="w-[15%] md:w-[8%] lg:w-[16%] xl:w-[15%] bg-red-200 p-4">
+      <div className="w-[15%] md:w-[8%] lg:w-[16%] xl:w-[15%] p-4">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
-            <Image src="/LogoSchool.png" alt="logo" width={40} height={40} />
+            <Image src="/LogoSchool.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block">SchoolMed</span>
         </Link>
+        <Menu />
       </div>
       {/*Right Side*/}
-      <div className="w-[85%] md:w-[92%] lg:w-[84%] xl:w-[85%] bg-gray-900 text-white p-4">
-        right side
+      <div className="w-[85%] md:w-[92%] lg:w-[84%] xl:w-[85%] bg-[#F7F8FA] overflow-auto">
+        <Navbar />
+        {children}
       </div>
+
+
 
     </div>
 
